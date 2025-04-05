a54b5ad51304fe30c1fa728badbfad302c2309ea
Justice <unankajustice@gmail.com>
2025-04-05 15:46:42 +0100

    update copyright year display in footer; clean up commented code in script.js

--- 07-Omnifood-Desktop/js/script.js
+++ 07-Omnifood-Desktop/js/script.js
@@ -2,14 +2,12 @@ console.log('Hello world!')
 
 const myName = 'Jonas Schmedtmann'
 const h1 = document.querySelector('.heading-primary')
-console.log(myName)
-console.log(h1)
 
-// h1.addEventListener("click", function () {
-//   h1.textContent = myName;
-//   h1.style.backgroundColor = "red";
-//   h1.style.padding = "5rem";
-// });
+// h1.addEventListener('click', function () {
+//   h1.textContent = myName
+//   h1.style.backgroundColor = 'red'
+//   h1.style.padding = '5rem'
+// })
 
 ///////////////////////////////////////////////////////////
 // Set current year
